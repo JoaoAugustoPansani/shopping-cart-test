1a8e392cc6f9dec9148d89c7317f7b2ac2f3ef1d
Joao Pansani <joaoaugustom.pansani@gmail.com>
2022-04-27 11:49:50 -0300

    Adding tests and removing useless endpoints

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const route = express.Router();
-const { getProducts, getSingleProduct, createProduct } = require('../controllers/productController');
+const { getProducts } = require('../controllers/productController');
 
 //PRODUCT API ENDPOINTS
 //Endpoint to list all products
 route.get('/', getProducts);
-
-//Endpoint to list specific product
-route.get('/:id', getSingleProduct);
-
-//Endpoint do create new product 
-route.post('/', createProduct);
 
 module.exports = route;
