63c916e6ad66a6c6888f0ec3b247f9192f8270a6
Joao Pansani <joaoaugustom.pansani@gmail.com>
2022-04-27 10:35:30 -0300

    chore: update product controllers functions in route args

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,34 +1,15 @@
 const express = require('express');
 const route = express.Router();
-const Product = require('../models/Product');
+const { getProducts, getSingleProduct, createProduct } = require('../controllers/productController');
 
 //PRODUCT API ENDPOINTS
 //Endpoint to list all products
-route.get('/', async (req, res) => {
-    const products = await Product.findAll();
-    res.send(products);
-});
+route.get('/', getProducts);
 
 //Endpoint to list specific product
-route.get('/:id', async (req, res) => {
-    const requestedId = req.params.id;
-    const product = await Product.findOne({ where: { id: requestedId } });
-    res.send(product);
-});
+route.get('/:id', getSingleProduct);
 
 //Endpoint do create new product 
-route.post('/', async (req, res) => {
-    await Product.create(req.body);
-    res.send('Product created!')
-});
-
-//Endpoint do update a product 
-// route.put('/:id', async (req, res) => {
-//     const requestedId = req.params.id;
-//     const product = await Product.findOne({where : {id: requestedId}});
-// product.available_units = req.body.available_units;
-// await product.save();
-// res.send('Available units updated!')
-// });
+route.post('/', createProduct);
 
 module.exports = route;
