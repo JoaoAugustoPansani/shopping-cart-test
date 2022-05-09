9e298f0c10862984b64a5f96178021ecda2bb630
Joao Pansani <joaoaugustom.pansani@gmail.com>
2022-05-09 11:52:19 -0300

    Converting database from sqlite to mysql

--- database/database.js
+++ database/database.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  host: "./dev.sqlite",
+const sequelize = new Sequelize('shopping-cart', 'root', 'pansani123',{
+  dialect: "mysql",
+  host: "localhost",
+  port: 3306
 });
 
 module.exports = sequelize;
