488e5a9ec170149da7384cbf1329984775bee98b
Joao Pansani <joaoaugustom.pansani@gmail.com>
2022-05-16 16:57:06 -0300

    Deleting unnecessary describe from cartService test

--- tests/services/cartService.test.js
+++ tests/services/cartService.test.js
@@ -6,32 +6,30 @@ describe("CartSevice", () => {
     await Cart.destroy({ where: {} });
   });
 
-  describe("receive user_session_id token", () => {
-    describe("when token is not null", () => {
-      it("should return success true, success message and generate cart", async () => {
-        const userSessionId = "jfka3io*_4";
-        const result = await generateCartService(userSessionId);
+  describe("when token is not null", () => {
+    it("should return success true, success message and generate cart", async () => {
+      const userSessionId = "jfka3io*_4";
+      const result = await generateCartService(userSessionId);
 
-        expect(result.success).toBe(true);
-        expect(result.message).toEqual("User cart available!");
-        console.log(typeof result.data[0].id);
-        expect(result.data[0].dataValues).toEqual({
-          subtotal_in_cents: 0,
-          total_in_cents: 0,
-          id: expect.any(Number),
-          user_session_id: userSessionId,
-        });
+      expect(result.success).toBe(true);
+      expect(result.message).toEqual("User cart available!");
+      console.log(typeof result.data[0].id);
+      expect(result.data[0].dataValues).toEqual({
+        subtotal_in_cents: 0,
+        total_in_cents: 0,
+        id: expect.any(Number),
+        user_session_id: userSessionId,
       });
     });
+  });
 
-    describe("when token is null", () => {
-      it("should return success false and a error message", async () => {
-        const userSessionId = null;
-        const result = await generateCartService(userSessionId);
+  describe("when token is null", () => {
+    it("should return success false and a error message", async () => {
+      const userSessionId = null;
+      const result = await generateCartService(userSessionId);
 
-        expect(result.success).toBe(false);
-        expect(result.message).toEqual("The session id token can't be null.");
-      });
+      expect(result.success).toBe(false);
+      expect(result.message).toEqual("The session id token can't be null.");
     });
   });
 });
