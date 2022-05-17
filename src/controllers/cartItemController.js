29b4b8179aa21acdb3e756899b80b3a034b4e699
Joao Pansani <joaoaugustom.pansani@gmail.com>
2022-05-17 10:57:20 -0300

    Create service to add cart item and test for cart item service

---
+++ src/controllers/cartItemController.js
@@ -0,0 +1,23 @@
+const { addToCartService } = require("../services/cartItemService");
+
+const addToCart = async (req, res) => {
+    const userSessionId = req.body.user_session_id;
+    const productId = req.body.product_id;
+    const result = await addToCartService(userSessionId, productId);
+
+    if (result.success) {
+        res.send({
+            message: result.message,
+            data: result.data
+        });
+    } else {
+        res.status(400);
+        res.send({
+            message: result.message
+        });
+    }
+};
+
+module.exports = {
+    addToCart
+}
